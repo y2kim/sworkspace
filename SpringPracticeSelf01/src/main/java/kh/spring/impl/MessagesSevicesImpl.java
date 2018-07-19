7b54374f1218c5ef37a22adb968c9e6d2d1a60a3
y2kim <kchy0806@naver.com>
2018-07-19 17:24:44 +0900

    transaction 처리
    how gee

--- SpringPracticeSelf01/src/main/java/kh/spring/impl/MessagesSevicesImpl.java
+++ SpringPracticeSelf01/src/main/java/kh/spring/impl/MessagesSevicesImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import kh.spring.dto.MessagesDTO;
 import kh.spring.interfaces.MessagesDAO;
@@ -16,24 +17,29 @@ public class MessagesSevicesImpl implements MessagesServices {
 	private  MessagesDAO dao ;
 	
 	@Override
+	@Transactional
+	//2개이상 작업이 껴있는  경우 트랜잭션 사용 
 	public int insertMessages(MessagesDTO dto) {
 		
 		return dao.insertMessages(dto);
 	}
 
 	@Override
+	@Transactional
 	public List<MessagesDTO> selectMessages() {
 		
 		return dao.selectMessages();
 	}
 
 	@Override
+	@Transactional
 	public int deleteMessages(int seq) {
 
 		return dao.deleteMessages(seq);
 	}
 
 	@Override
+	@Transactional
 	public int modifyMessages(MessagesDTO dto) {
 
 		return dao.modifyMessages(dto);
